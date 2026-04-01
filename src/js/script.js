cebe6fa6c5321d40193bffca7e7fb07119c5ce88
Andrew <wotriger@gmail.com>
2026-04-01 23:25:45 +0200

    Created recent.html + js for news

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,19 @@
+import Swiper from 'swiper'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { Pagination } from 'swiper/modules'
 import '/src/sass/style.scss'
 
 window.addEventListener('DOMContentLoaded', () => {
+	const reviewSwiper = new Swiper('.review__slider', {
+		direction: 'horizontal',
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		modules: [Pagination],
+	})
 	class cardsItem {
 		constructor(parent, src, sub, title, color, subColor) {
 			this.parent = document.querySelector(parent)
@@ -444,6 +457,93 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	teamItems.forEach(item => {
 		const card = new TeamCards(item)
+		card.render()
+	})
+
+	class NewsCards {
+		constructor({ parent, date, author, title, text }) {
+			this.parent = document.querySelector(parent)
+			this.date = date
+			this.author = author
+			this.title = title
+			this.text = text
+		}
+
+		render() {
+			if (!this.parent) return
+
+			const element = document.createElement('li')
+			element.classList.add('news__blog-item')
+
+			element.innerHTML = `
+			<span class="news__blog-tag title-lg title-blue">${this.date}</span>
+			<div class="news__blog-info">
+				<div class="news__blog-author title-md title-blue">
+					${this.author}
+				</div>
+				<h3 class="news__blog-title title-lg title-blue">
+					${this.title}
+				</h3>
+				<div class="news__blog-text text-md">
+					${this.text}
+				</div>
+				<button class="news__blog-btn button-yellow">
+					Read More<img src="/src/img/btn_arrow.svg" alt="" />
+				</button>
+			</div>
+			`
+
+			this.parent.append(element)
+		}
+	}
+
+	const newsCards = [
+		{
+			parent: '.recent__blog',
+			date: '30 oct',
+			author: 'By Andrii Bezuhlyi',
+			title: 'The Benefits of Vitamin D & How to Get It',
+			text: 'Simply dummy text of the printing and typesetting industry.Lorem Ipsum',
+		},
+		{
+			parent: '.recent__blog',
+			date: '30 oct',
+			author: 'By Andrii Bezuhlyi',
+			title: 'The Benefits of Vitamin D & How to Get It',
+			text: 'Simply dummy text of the printing and typesetting industry.Lorem Ipsum',
+		},
+		{
+			parent: '.recent__blog',
+			date: '30 oct',
+			author: 'By Andrii Bezuhlyi',
+			title: 'The Benefits of Vitamin D & How to Get It',
+			text: 'Simply dummy text of the printing and typesetting industry.Lorem Ipsum',
+		},
+		{
+			parent: '.recent__blog',
+			date: '30 oct',
+			author: 'By Andrii Bezuhlyi',
+			title: 'The Benefits of Vitamin D & How to Get It',
+			text: 'Simply dummy text of the printing and typesetting industry.Lorem Ipsum',
+		},
+		{
+			parent: '.recent__blog',
+			date: '30 oct',
+			author: 'By Andrii Bezuhlyi',
+			title: 'The Benefits of Vitamin D & How to Get It',
+			text: 'Simply dummy text of the printing and typesetting industry.Lorem Ipsum',
+		},
+		{
+			parent: '.recent__blog',
+			date: '30 oct',
+			author: 'By Andrii Bezuhlyi',
+			title: 'The Benefits of Vitamin D & How to Get It',
+			text: 'Simply dummy text of the printing and typesetting industry.Lorem Ipsum',
+		},
+	]
+
+	newsCards.forEach(item => {
+		const card = new NewsCards(item)
 		card.render()
 	})
 })
