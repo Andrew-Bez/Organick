e20730e1ee9f2a5ad2054d2d425524b12b651f4f
Andrew <wotriger@gmail.com>
2026-04-11 19:54:39 +0200

    update product.js

--- src/js/components/products.js
+++ src/js/components/products.js
@@ -1,8 +1,7 @@
 import { getImageUrl } from '../utils/getImageURL'
 
 class productsItem {
-	constructor({ parent, tag, src, alt, title, priceOld, priceNew }) {
-		this.parent = document.querySelector(parent)
+	constructor({ tag, src, alt, title, priceOld, priceNew }) {
 		this.tag = tag
 		this.src = src
 		this.alt = alt
@@ -12,10 +11,10 @@ class productsItem {
 	}
 
 	render() {
-		if (!this.parent) return
 		const element = document.createElement('li')
 		element.classList.add('products__item')
-		element.innerHTML = `<div class="products__tag text-sm">${this.tag}</div>
+		element.innerHTML = `<a href='/shop-single.html'>
+		<div class="products__tag text-sm">${this.tag}</div>
 							<img
 								src=${getImageUrl(this.src)}
 								alt=${this.alt}
@@ -35,237 +34,35 @@ class productsItem {
 									alt="five stars"
 									class="products__rating"
 								/>
-							</div>`
+							</div></a>`
+		return element
+	}
+}
 
-		this.parent.append(element)
+async function renderProducts(containerSelector, limit = 8) {
+	const container = document.querySelector(containerSelector)
+	if (!container) return
+
+	container.innerHTML = ''
+
+	try {
+		const response = await fetch('http://localhost:3000/products')
+		const data = await response.json()
+
+		const productsToShow = data.slice(0, limit)
+
+		productsToShow.forEach(product => {
+			const card = new productsItem(product)
+			container.appendChild(card.render())
+		})
+	} catch (error) {
+		console.error('Помилка завантаження товарів:', error)
 	}
 }
 
 export function initProducts() {
-	const categoriesItems = [
-		{
-			parent: '.categories__list',
-			tag: 'Vegetable',
-			src: 'broccoli.png',
-			alt: 'broccoli',
-			title: 'Calabrese Broccoli',
-			priceOld: '$20.00',
-			priceNew: '$13.00',
-		},
-		{
-			parent: '.categories__list',
-			tag: 'Fresh',
-			src: 'banana.png',
-			alt: '"banana"',
-			title: 'Fresh Banana Fruites',
-			priceOld: '$20.00',
-			priceNew: '$14.00',
-		},
-		{
-			parent: '.categories__list',
-			tag: 'Millets',
-			src: 'pistaches.png',
-			alt: '"pistaches"',
-			title: 'White Nuts',
-			priceOld: '$20.00',
-			priceNew: '$15.00',
-		},
-		{
-			parent: '.categories__list',
-			tag: 'Vegetable',
-			src: 'tomato.png',
-			alt: '"tomato"',
-			title: 'Vegan Red Tomato',
-			priceOld: '$20.00',
-			priceNew: '$17.00',
-		},
-		{
-			parent: '.categories__list',
-			tag: 'Health',
-			src: 'bean.png',
-			alt: '"bean"',
-			title: 'Mung Bean',
-			priceOld: '$20.00',
-			priceNew: '$11.00',
-		},
-		{
-			parent: '.categories__list',
-			tag: 'Nuts',
-			src: 'hazelnut.png',
-			alt: '"hazelnut"',
-			title: 'Brown Hazelnut',
-			priceOld: '$20.00',
-			priceNew: '$12.00',
-		},
-		{
-			parent: '.categories__list',
-			tag: 'Fresh',
-			src: 'eggs.png',
-			alt: '"eggs"',
-			title: 'Eggs',
-			priceOld: '$20.00',
-			priceNew: '$17.00',
-		},
-		{
-			parent: '.categories__list',
-			tag: 'Fresh',
-			src: 'bread.png',
-			alt: '"bread"',
-			title: 'Zelco Suji Elaichi Rusk',
-			priceOld: '$20.00',
-			priceNew: '$15.00',
-		},
-	]
-	const offerItems = [
-		{
-			parent: '.offer__products',
-			tag: 'Vegetable',
-			src: 'mung-bean.png',
-			alt: 'mung bean',
-			title: 'Mung Bean',
-			priceOld: '$20.00',
-			priceNew: '$11.00',
-		},
-		{
-			parent: '.offer__products',
-			tag: 'Vegetable',
-			src: 'brown-hazelnut.png',
-			alt: 'Brown Hazelnut',
-			title: 'Brown Hazelnut',
-			priceOld: '$20.00',
-			priceNew: '$12.00',
-		},
-		{
-			parent: '.offer__products',
-			tag: 'Vegetable',
-			src: 'onion.png',
-			alt: 'Onion',
-			title: 'Onion',
-			priceOld: '$20.00',
-			priceNew: '$17.00',
-		},
-		{
-			parent: '.offer__products',
-			tag: 'Vegetable',
-			src: 'cabbage.png',
-			alt: 'Cabbage',
-			title: 'Cabbage',
-			priceOld: '$20.00',
-			priceNew: '$17.00',
-		},
-	]
-	const shopItems = [
-		{
-			parent: '.shop__products',
-			tag: 'Vegetable',
-			src: 'broccoli.png',
-			alt: 'broccoli',
-			title: 'Calabrese Broccoli',
-			priceOld: '$20.00',
-			priceNew: '$13.00',
-		},
-		{
-			parent: '.shop__products',
-			tag: 'Fresh',
-			src: 'banana.png',
-			alt: '"banana"',
-			title: 'Fresh Banana Fruites',
-			priceOld: '$20.00',
-			priceNew: '$14.00',
-		},
-		{
-			parent: '.shop__products',
-			tag: 'Millets',
-			src: 'pistaches.png',
-			alt: '"pistaches"',
-			title: 'White Nuts',
-			priceOld: '$20.00',
-			priceNew: '$15.00',
-		},
-		{
-			parent: '.shop__products',
-			tag: 'Vegetable',
-			src: 'tomato.png',
-			alt: '"tomato"',
-			title: 'Vegan Red Tomato',
-			priceOld: '$20.00',
-			priceNew: '$17.00',
-		},
-		{
-			parent: '.shop__products',
-			tag: 'Health',
-			src: 'bean.png',
-			alt: '"bean"',
-			title: 'Mung Bean',
-			priceOld: '$20.00',
-			priceNew: '$11.00',
-		},
-		{
-			parent: '.shop__products',
-			tag: 'Nuts',
-			src: 'hazelnut.png',
-			alt: '"hazelnut"',
-			title: 'Brown Hazelnut',
-			priceOld: '$20.00',
-			priceNew: '$12.00',
-		},
-		{
-			parent: '.shop__products',
-			tag: 'Fresh',
-			src: 'eggs.png',
-			alt: '"eggs"',
-			title: 'Eggs',
-			priceOld: '$20.00',
-			priceNew: '$17.00',
-		},
-		{
-			parent: '.shop__products',
-			tag: 'Fresh',
-			src: 'bread.png',
-			alt: '"bread"',
-			title: 'Zelco Suji Elaichi Rusk',
-			priceOld: '$20.00',
-			priceNew: '$15.00',
-		},
-		{
-			parent: '.shop__products',
-			tag: 'Vegetable',
-			src: 'mung-bean.png',
-			alt: 'mung bean',
-			title: 'Mung Bean',
-			priceOld: '$20.00',
-			priceNew: '$11.00',
-		},
-		{
-			parent: '.shop__products',
-			tag: 'Vegetable',
-			src: 'brown-hazelnut.png',
-			alt: 'Brown Hazelnut',
-			title: 'Brown Hazelnut',
-			priceOld: '$20.00',
-			priceNew: '$12.00',
-		},
-		{
-			parent: '.shop__products',
-			tag: 'Vegetable',
-			src: 'onion.png',
-			alt: 'Onion',
-			title: 'Onion',
-			priceOld: '$20.00',
-			priceNew: '$17.00',
-		},
-		{
-			parent: '.shop__products',
-			tag: 'Vegetable',
-			src: 'cabbage.png',
-			alt: 'Cabbage',
-			title: 'Cabbage',
-			priceOld: '$20.00',
-			priceNew: '$17.00',
-		},
-	]
-
-	categoriesItems.forEach(item => new productsItem(item).render())
-	offerItems.forEach(item => new productsItem(item).render())
-	shopItems.forEach(item => new productsItem(item).render())
+	renderProducts('.categories__list', 8)
+	renderProducts('.offer__products', 4)
+	renderProducts('.shop-single__list', 4)
+	renderProducts('.shop__products', 12)
 }
